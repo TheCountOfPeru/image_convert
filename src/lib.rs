c158b58b0b6e25993160972d6b423a6a9c408f40
Jonathan Yee <jonathan.yee16@gmail.com>
2021-07-27 19:17:24 -0600

    added clap

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,25 @@
-use std::error::Error;
-use std::fs;
-use std::env;
-pub struct Config {
-    pub query: String,
-    pub filename: String,
-}
-impl Config {
-    pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
-        args.next();
+// use std::env;
+// pub struct Config {
+//     pub file_extension: String,
+//     pub filename: String,
+// }
+// impl Config {
+//     pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
+//         args.next();
 
-        let file_extension = match args.next() {
-            Some(arg) => arg,
-            None => return Err("Didn't get a query string"),
-        };
+//         let file_extension = match args.next() {
+//             Some(arg) => arg,
+//             None => return Err("Didn't get a file extension string"),
+//         };
 
-        let filename = match args.next() {
-            Some(arg) => arg,
-            None => return Err("Didn't get a file name"),
-        };
+//         let filename = match args.next() {
+//             Some(arg) => arg,
+//             None => return Err("Didn't get a file name"),
+//         };
 
-        Ok(Config {
-            query,
-            filename,
-            case_sensitive,
-        })
-    }
-}
+//         Ok(Config {
+//             file_extension,
+//             filename,
+//         })
+//     }
+// }
